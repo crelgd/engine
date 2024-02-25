2f9671c344f92a0447f0278297d07565fda1bcf8
crelgd <decrelgd@gmail.com>
2024-02-25 14:45:28 +0200

    Add description, change metod name, v1.0

--- ui/label.py
+++ ui/label.py
@@ -1,14 +1,59 @@
-# Манипуляция текстом
+# Название: Код для управления текстом
+# 
+# Описание:
+# Этот код содержит класс Label для создания и управления текстом
+# Он предоставляет основные методы управления текстом:
+# - расположения текстом
+# - и отображения текста
+# 
+# Автор: crelgd <decrelgd@gmail.com>
+# 
+# Лицензия:
+# MIT License
+# 
+# Copyright (c) 2024 crelgd
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from .window import Window 
 import pygame as p
 
+# Инициализация pygame
 p.init()
 
-# Класс со всемі методами/функциями для текста
 class Label(Window):
-    # Инициализация данный
+    """
+    Класс для создания и упралением текстом
+    """
+
     def __init__(self, text='Label', size = 16, font_name='Roboto'):
+        """
+        Инициализация окна.
+
+        Параметры:
+        - text (str): Текст который будет отображатся.
+        - size (int): Размер текста.
+        - font_name (int): Название шрифта.
+
+        * ШРИФТЫ КОТОРЫХ НЕТ В ОС НЕ БУДУТ ОТОБРАЖАТЬСЯ!
+        """
+
         super().__init__()
 
         self.text = text 
@@ -16,18 +61,33 @@ class Label(Window):
         self.font_size = size
         self.image = ''
 
+        # Инициализация рамположения текста
         self.x = 0
         self.y = 0
 
+        # Инициализация шрифтов
+        # Принимает параметры: 
+        # - self.font_name: Название шрифта
+        # - self.font_size: Размер шрифта
         self.font = p.font.SysFont(self.font_name, self.font_size)
 
-    # Показывание обьекта
     def render(self):
+        """
+        Отображение обьетов
+        """
+
         self.image = self.font.render(self.text, True, (0, 0, 0))
         self.win.blit(self.image, (self.x, self.y))
 
-    # Изменение координат обьекта
     def changeCoord(self, x, y):
+        """
+        Изменение расположения обьекта
+
+        Параметры:
+        - x (int): Новая ширина окна.
+        - y (int): Новая высота окна.
+        """
+
         self.x = x
         self.y = y 
 
--- ui/window.py
+++ ui/window.py
@@ -1,18 +1,56 @@
-# Отвечает для создания окна
-# В других файлах будет продожение этого файла
+# Название: Код для управления окном 
+# 
+# Описание:
+# Этот код содержит класс Window для создания окна 
+# Он предоставляет основные методы для управления окном, фоном и отображением обьектов
+# 
+# Автор: crelgd <decrelgd@gmail.com>
+# 
+# Лицензия:
+# MIT License
+# 
+# Copyright (c) 2024 crelgd
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 from pygame import *
 import os
 
-# Получить путь к директории, где находится этот скрипт
+# Получаем путь к директории, где находится этот скрипт
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Изменить текущую рабочую директорию на директорию с файлом
+# Изменяем текущую рабочую директорию на директорию с файлом
 os.chdir(os.path.join(script_dir, 'res'))
 
 class Window:
-    # Инициализация данный
+    """
+    Класс для создания и управления игровым окном.
+    """
+
     def __init__(self, winTitle='Engine'):
+        """
+        Инициализация окна.
+
+        Параметры:
+        - winTitle (str): Название окна.
+        """
+
         self.x = 300
         self.y = 200
         self.winTitle = winTitle
@@ -21,40 +59,71 @@ class Window:
         self.clock = time.Clock()
         self.run = True
 
+        # Создаем окно с размерами self.x, self.y
         self.win = display.set_mode((self.x, self.y))
 
+        # Загружаем фоновое изображение и изменяем его размеры под размеры окна
         self.background = transform.scale(image.load("bg.png"), (self.x, self.y))
 
+        # Устанавливаем иконку окна
         display.set_icon(image.load('ico.png'))
 
-    # Дает возможноть менять название окна
     def setTitle(self, title):
+        """
+        Устанавливает заголовок окна.
+
+        Параметры:
+        - title (str): Новый заголовок окна.
+        """
+
         display.set_caption(title)
 
-    # Дает возможность изменять фон
     def setBG(self, obj):
+        """
+        Устанавливает новое фоновое изображение окна.
+
+        Параметры:
+        - obj): Новое фоновое изображение.
+        """
+
         self.background = transform.scale(obj, (self.x, self.y))
 
-    # Изменение размера окна
-    def reSize(self, x, y):
+    def resize(self, x, y):
+        """
+        Изменяет размеры окна.
+
+        Параметры:
+        - x (int): Новая ширина окна.
+        - y (int): Новая высота окна.
+        """
+
         self.x = x
         self.y = y
         self.win = display.set_mode((x, y))
         self.background = transform.scale(self.background, (x, y))
 
-    # Создает окно
     def create(self):
+        """Создает окно с текущими настройками."""
+
         display.set_caption(self.winTitle)
 
-    # Рисование обьектов
-    def winLoop(self, draw_functions=None): 
+    def winLoop(self, draw_functions=None):
+        """
+        Основной цикл обновления окна.
+
+        Параметры:
+        - draw_functions: Список функций для отрисовки дополнительных объектов.
+        """
+
         while self.run:
             for e in event.get():
                 if e.type == QUIT:
                     self.run = False
 
+            # Отрисовка фонового изображения
             self.win.blit(self.background, (0, 0))
             
+            # Вызов функций отрисовки
             if draw_functions: 
                 for func in draw_functions:
                     func()  
@@ -62,11 +131,10 @@ class Window:
             display.update()
             self.clock.tick(self.FPS)
 
-# Просто тест написаного
+# Тестирование
 if __name__ == '__main__':
     window = Window()
     window.create()
     # Тут код...
     window.setBG(image.load("res/bg.png"))
     window.winLoop()
-
